2bbd190269cc01142215ac60e7cc93a618671e67
wolf <124054899+wolfohgiraffers@users.noreply.github.com>
2025-09-14 09:04:01 +0900

    Update 00.variables.tf

--- 00.variables.tf
+++ 00.variables.tf
@@ -2,3 +2,8 @@ variable "AWS_ACCESS_KEY_ID" {
   description = "AWS access key"
   type        = string
 }
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
